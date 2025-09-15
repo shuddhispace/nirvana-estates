be8be1443d7d807861258ac9b3b2142fcfcfa4e5
unknown <shuddhispace@gmail.com>
2025-09-15 11:04:57 +0530

    Add dynamic sitemap.xml route and robots.txt

--- server.js
+++ server.js
@@ -152,5 +152,42 @@ app.post("/submit-seller", async (req, res) => {
   }
 });
 
+// Dynamic sitemap.xml route
+app.get('/sitemap.xml', (req, res) => {
+  const baseUrls = [
+    'https://nirvanaestates.co.in/',
+    'https://nirvanaestates.co.in/about.html',
+    'https://nirvanaestates.co.in/contact.html',
+    'https://nirvanaestates.co.in/properties.html'
+  ];
+
+  const dataFile = 'data/properties.json';
+  let propertyUrls = [];
+
+  if (fs.existsSync(dataFile)) {
+    const fileContent = fs.readFileSync(dataFile, 'utf-8');
+    if (fileContent) {
+      const properties = JSON.parse(fileContent);
+      propertyUrls = properties.map(p => `https://nirvanaestates.co.in/properties/${p.id}`);
+    }
+  }
+
+  const urls = [...baseUrls, ...propertyUrls];
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls.map(url => `
+  <url>
+    <loc>${url}</loc>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>`).join('')}
+</urlset>`;
+
+  res.header('Content-Type', 'application/xml');
+  res.send(sitemap);
+});
+
+
 // Start server
 app.listen(3000, () => console.log("Server running on port 3000"));
