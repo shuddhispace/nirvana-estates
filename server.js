0ffc6a5bc09e9b6788f24d56b8e74a29b0c7d252
unknown <shuddhispace@gmail.com>
2025-12-16 17:55:39 +0530

    server

--- server.js
+++ server.js
@@ -19,6 +19,22 @@ app.use(express.static('public'));
   if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
 });
 
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+
+const uploadDir = path.join(__dirname, "public/uploads/images");
+fs.mkdirSync(uploadDir, { recursive: true });
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
 
 const upload = multer({ storage: storage });
 
